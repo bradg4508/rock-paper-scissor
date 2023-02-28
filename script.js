1401dfcab307a5f15373177bccc635883a1e80ab
bradg4508 <64989476+bradg4508@users.noreply.github.com>
2023-02-28 17:56:22 -0600

    Create function to play rounds of the game

--- script.js
+++ script.js
@@ -27,18 +27,46 @@ function getPlayerChoice() {
 function playRound (playerPick, computerPick) {
     let playerPickCap = playerPick.charAt(0).toUpperCase() + playerPick.substring(1);
     let computerPickCap = computerPick.charAt(0).toUpperCase() + computerPick.substring(1);
-    if ((playerPickCap === "Rock" && computerPickCap === "Scissors") ||
-        (playerPickCap === "Paper" && computerPickCap === "Rock") ||
-        (playerPickCap === "Scissors" && computerPickCap === "Paper")) {
-        return `You Win! ${playerPickCap} beats ${computerPickCap}.`;
-    } else if (playerPickCap === computerPickCap) {
+
+    if (playerPickCap === computerPickCap) {
         return `It's a tie! You both chose ${playerPickCap}.`
+    } else if ((playerPickCap === "Rock" && computerPickCap === "Scissors") 
+    || (playerPickCap === "Paper" && computerPickCap === "Rock") 
+    || (playerPickCap === "Scissors" && computerPickCap === "Paper")) {
+        return `You Win! ${playerPickCap} beats ${computerPickCap}.`;
     } else {
         return `You Lose! ${computerPickCap} beats ${playerPickCap}.`;
     }
 }
 
-// Test
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// Create a function to call the playRound() function to simulate a game with multiple rounds
+// The game will consist of 5 rounds that provides updates of the score after each round
+// Once all rounds are complete, the game will tell the player the final score and whether or not they won
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        if (roundResult.indexOf("Win") >= 0) {
+            playerScore++;
+        } else if (roundResult.indexOf("Lose") >= 0) {
+            computerScore++;
+        }
+        console.log(roundResult);
+        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+    }
+    let finalResult = `Final Score\nPlayer: ${playerScore} Computer: ${computerScore}`;
+    if (playerScore === computerScore) {
+        console.log(`${finalResult}\nThe computer is truly your equal.`);
+    } else if (playerScore > computerScore) {
+        console.log(`${finalResult}\nCongratulations, you have defeated the computer!`);
+    } else {
+        console.log(`${finalResult}\nThe computer has bested you this time.`);
+    }
+}
+
+// Play the game for 5 rounds
+game();
