5dff2f90680e5feae2b06854791c3fb3a7b385ee
bradg4508 <64989476+bradg4508@users.noreply.github.com>
2023-03-09 09:41:14 -0600

    Respond to button click and display results in div

--- script.js
+++ script.js
@@ -5,68 +5,66 @@
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
-        return "rock";
+        return "Rock";
     } else if (choice === 2) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
-}
-
-// Create a function to get a player's choice of rock. paper, or scissors
-// The selection will be input from the player
-function getPlayerChoice() {
-    const choice = prompt("What would you like to choose: Rock, Paper, or Scissors", "");
-    return choice.toLowerCase();
 }
 
 // Create a function that plays a single round of rock, paper, scissors
 // The function should take two parameters - the playerSelection and computerSelection
 // It will then return a string that declares the result of the round like so: "You Lose! Paper beats Rock"
 // There are responses for when the player either wins, loses, or ties
-function playRound (playerPick, computerPick) {
-    const playerPickCap = playerPick.charAt(0).toUpperCase() + playerPick.substring(1);
-    const computerPickCap = computerPick.charAt(0).toUpperCase() + computerPick.substring(1);
-
-    if (playerPickCap === computerPickCap) {
-        return `It's a tie! You both chose ${playerPickCap}.`
-    } else if ((playerPickCap === "Rock" && computerPickCap === "Scissors") || 
-               (playerPickCap === "Paper" && computerPickCap === "Rock") || 
-               (playerPickCap === "Scissors" && computerPickCap === "Paper")) {
-        return `You Win! ${playerPickCap} beats ${computerPickCap}.`;
+function playRound(playerPick, computerPick) {
+    if (playerPick=== computerPick) {
+        return `It's a tie! You both chose ${playerPick}.`
+    } else if ((playerPick === "Rock" && computerPick === "Scissors") || 
+               (playerPick === "Paper" && computerPick === "Rock") || 
+               (playerPick === "Scissors" && computerPick === "Paper")) {
+        return `You Win! ${playerPick} beats ${computerPick}.`;
     } else {
-        return `You Lose! ${computerPickCap} beats ${playerPickCap}.`;
+        return `You Lose! ${computerPick} beats ${playerPick}.`;
     }
 }
 
-// Create a function to call the playRound() function to simulate a game with multiple rounds
-// The game will consist of 5 rounds that provide updates of the score after each round
-// Once all rounds are complete, the game will tell the player the final score and whether or not they won
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
+// Get a player's choice of rock, paper, or scissors based on the button that is pressed
+// Play the user's choice against the computer's choice by calling the playRound() function
+// Display the running score, and announce a winner of the game once one player reaches 5 points
+// In the end, it will tell the player the final score and whether or not they won
+function game(e) {
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult.indexOf("Win") >= 0) {
+        playerScore++;
+    } else if (roundResult.indexOf("Lose") >= 0) {
+        computerScore++;
+    }
+    
+    const div = document.querySelector("div");
+    // Style the text content in the div to accept line breaks for cleaner output on the screen
+    div.style.cssText = "white-space: pre";
+    const newLine = "\r\n";
+    const currentScore = `PLAYER: ${playerScore}${newLine}COMPUTER: ${computerScore}`;
+    div.textContent = `${roundResult}${newLine}${currentScore}`;
+    
+    if (playerScore === 5 || computerScore === 5) {
+        const buttons = document.querySelectorAll("button");
+        buttons.forEach((button) => button.removeEventListener("click", game));
+    }
 
-        if (roundResult.indexOf("Win") >= 0) {
-            playerScore++;
-        } else if (roundResult.indexOf("Lose") >= 0) {
-            computerScore++;
-        }
-        console.log(roundResult);
-        console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-    }
-    const finalResult = `Final Score\nPlayer: ${playerScore} Computer: ${computerScore}`;
-    if (playerScore === computerScore) {
-        console.log(`${finalResult}\nThe computer is truly your equal.`);
-    } else if (playerScore > computerScore) {
-        console.log(`${finalResult}\nCongratulations, you have defeated the computer!`);
-    } else {
-        console.log(`${finalResult}\nThe computer has bested you this time.`);
-    }
+    const finalResult = `GAME OVER!${newLine}${currentScore}`;
+    if (playerScore >= 5) {
+        div.textContent = `${finalResult}${newLine}Congratulations, you have defeated the computer!`;
+    } else if (computerScore >= 5) {
+        div.textContent = `${finalResult}${newLine}The computer has bested you this time.`;
+    }    
 }
 
-// Play the game for 5 rounds
-game();
+// Call the game() function to play the user's button choice against the computer
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", game));
